808d66d5480522a8a48e0fc9fb0ecca86db40140
Han_Han <2088113262@qq.com>
2023-11-26 21:39:12 +0800

    完成db_conn和store

--- be/model/db_conn.py
+++ be/model/db_conn.py
@@ -1,75 +1,38 @@
-# import store
-#
-#
-# class DBConn:
-#     def __init__(self):
-#         self.cur = store.get_db_conn()
-#
-#     def user_id_exist(self, user_id):
-#         self.cur.execute(
-#             "SELECT user_id FROM user WHERE user_id = %s", (user_id,)
-#         )
-#         row = self.cur.fetchone()
-#         if row is None:
-#             return False
-#         else:
-#             return True
-#
-#     def book_id_exist(self, store_id, book_id):
-#         self.cur.execute(
-#             "SELECT book_id FROM store WHERE store_id = %s AND book_id = %s",
-#             (store_id, book_id),
-#         )
-#         row = self.cur.fetchone()
-#         if row is None:
-#             return False
-#         else:
-#             return True
-#
-#     def store_id_exist(self, store_id):
-#         self.cur.execute(
-#             "SELECT store_id FROM user_store WHERE store_id = %s", (store_id,)
-#         )
-#         row = self.cur.fetchone()
-#         if row is None:
-#             return False
-#         else:
-#             return True
-
-import store
+from be.model import store
 
 
 class DBConn:
     def __init__(self):
-        self.conn = store.get_db_conn()
+        self.cur = store.get_db_conn()
 
     def user_id_exist(self, user_id):
-        cursor = self.conn.execute(
-            "SELECT user_id FROM user WHERE user_id = ?;", (user_id,)
+        self.cur.execute(
+            "SELECT user_id FROM user WHERE user_id = %s", (user_id,)
         )
-        row = cursor.fetchone()
+        row = self.cur.fetchone()
         if row is None:
             return False
         else:
             return True
 
     def book_id_exist(self, store_id, book_id):
-        cursor = self.conn.execute(
-            "SELECT book_id FROM store WHERE store_id = ? AND book_id = ?;",
+        self.cur.execute(
+            "SELECT book_id FROM store WHERE store_id = %s AND book_id = %s",
             (store_id, book_id),
         )
-        row = cursor.fetchone()
+        row = self.cur.fetchone()
         if row is None:
             return False
         else:
             return True
 
     def store_id_exist(self, store_id):
-        cursor = self.conn.execute(
-            "SELECT store_id FROM user_store WHERE store_id = ?;", (store_id,)
+        self.cur.execute(
+            "SELECT store_id FROM user_store WHERE store_id = %s", (store_id,)
         )
-        row = cursor.fetchone()
+        row = self.cur.fetchone()
         if row is None:
             return False
         else:
             return True
+
--- be/model/store.py
+++ be/model/store.py
@@ -1,140 +1,82 @@
-# import logging
-# import os
-# import sqlite3 as sqlite
-# import pymysql
-# import pymysql
-#
-#
-# class Store:
-#     database: str
-#
-#     def __init__(self, db_path):
-#         # self.database = os.path.join(db_path, "be.db")
-#         self.init_tables()
-#
-#     def init_tables(self):
-#         try:
-#             cur = self.get_db_conn()
-#
-#             cur.execute("""
-#                 CREATE TABLE IF NOT EXISTS user (
-#                     user_id TEXT PRIMARY KEY,
-#                     password TEXT NOT NULL,
-#                     balance INTEGER NOT NULL,
-#                     token TEXT,
-#                     terminal TEXT
-#                 )
-#             """)
-#
-#             cur.execute("""
-#                 CREATE TABLE IF NOT EXISTS user_store(
-#                     user_id TEXT,
-#                     store_id,
-#                     PRIMARY KEY(user_id, store_id)
-#                 )
-#             """)
-#
-#             cur.execute("""
-#                 CREATE TABLE IF NOT EXISTS store(
-#                     store_id TEXT,
-#                     book_id TEXT,
-#                     book_info TEXT,
-#                     stock_level INTEGER,
-#                     PRIMARY KEY(store_id, book_id)
-#                 )
-#             """)
-#
-#             cur.execute("""
-#                 CREATE TABLE IF NOT EXISTS new_order(
-#                     order_id TEXT PRIMARY KEY,
-#                     user_id TEXT,
-#                     store_id TEXT
-#                 )
-#             """)
-#
-#             cur.execute("""
-#                 CREATE TABLE IF NOT EXISTS new_order_detail(
-#                     order_id TEXT,
-#                     book_id TEXT,
-#                     count INTEGER,
-#                     price INTEGER,
-#                     PRIMARY KEY(order_id, book_id)
-#                 )
-#             """)
-#
-#         except pymysql.Error as e:
-#             logging.error(e)
-#             # cur.rollback()
-#
-#     def get_db_conn(self):
-#         # return sqlite.connect(self.database)
-#         db = pymysql.connect(host='localhost', user='root', passwd='root', port=3306)
-#         return db.cursor()
-#
-#
-# database_instance: Store = None
-#
-#
-# def init_database(db_path):
-#     global database_instance
-#     database_instance = Store(db_path)
-#
-#
-# def get_db_conn():
-#     global database_instance
-#     return database_instance.get_db_conn()
-
-
 import logging
 import os
 import sqlite3 as sqlite
+import pymysql
+import pymysql
 
 
 class Store:
     database: str
 
     def __init__(self, db_path):
-        self.database = os.path.join(db_path, "be.db")
+        # self.database = os.path.join(db_path, "be.db")
         self.init_tables()
 
     def init_tables(self):
         try:
-            conn = self.get_db_conn()
-            conn.execute(
-                "CREATE TABLE IF NOT EXISTS user ("
-                "user_id TEXT PRIMARY KEY, password TEXT NOT NULL, "
-                "balance INTEGER NOT NULL, token TEXT, terminal TEXT);"
-            )
+            cur = self.get_db_conn()
 
-            conn.execute(
-                "CREATE TABLE IF NOT EXISTS user_store("
-                "user_id TEXT, store_id, PRIMARY KEY(user_id, store_id));"
-            )
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS users(
+                    user_id VARCHAR(100) PRIMARY KEY,
+                    password VARCHAR(100) NOT NULL,
+                    balance INT NOT NULL,
+                    token TEXT,
+                    terminal TEXT
+                )
+            """)
 
-            conn.execute(
-                "CREATE TABLE IF NOT EXISTS store( "
-                "store_id TEXT, book_id TEXT, book_info TEXT, stock_level INTEGER,"
-                " PRIMARY KEY(store_id, book_id))"
-            )
+            # cur.execute("""
+            #     CREATE TABLE IF NOT EXISTS user_store(
+            #         user_id TEXT,
+            #         store_id,
+            #         PRIMARY KEY(user_id, store_id)
+            #     )
+            # """)
 
-            conn.execute(
-                "CREATE TABLE IF NOT EXISTS new_order( "
-                "order_id TEXT PRIMARY KEY, user_id TEXT, store_id TEXT)"
-            )
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS stores(
+                    store_id VARCHAR(100),
+                    user_id VARCHAR(100),
+                    book_id VARCHAR(100),
+                    book_info TEXT,
+                    stock_level INT,
+                    PRIMARY KEY(store_id, book_id)
+                )
+            """)
 
-            conn.execute(
-                "CREATE TABLE IF NOT EXISTS new_order_detail( "
-                "order_id TEXT, book_id TEXT, count INTEGER, price INTEGER,  "
-                "PRIMARY KEY(order_id, book_id))"
-            )
+            # cur.execute("""
+            #     CREATE TABLE IF NOT EXISTS new_order(
+            #         order_id TEXT PRIMARY KEY,
+            #         user_id TEXT,
+            #         store_id TEXT,
+            #         TTL DATETIME
+            #     )
+            # """)
 
-            conn.commit()
-        except sqlite.Error as e:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS orders(
+                    order_id VARCHAR(200),
+                    user_id VARCHAR(100),
+                    book_id VARCHAR(100),
+                    count INT,
+                    price INT,
+                    status VARCHAR(9),
+                    completion_time DATE,
+                    TTL DATETIME,
+                    PRIMARY KEY(order_id, book_id)
+                )
+            """)
+
+            cur.connection.commit()
+        except pymysql.Error as e:
             logging.error(e)
-            conn.rollback()
+            cur.connection.rollback()
 
-    def get_db_conn(self) -> sqlite.Connection:
-        return sqlite.connect(self.database)
+    def get_db_conn(self):
+        # return sqlite.connect(self.database)
+        db = pymysql.connect(host='localhost', user='root', passwd='root', port=3306)
+        return db.cursor()
 
 
 database_instance: Store = None
@@ -148,3 +90,4 @@ def init_database(db_path):
 def get_db_conn():
     global database_instance
     return database_instance.get_db_conn()
+
