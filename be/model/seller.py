b44d3d66d827c8a9300f8be28e9c9c9505121ee3
Han_Han <2088113262@qq.com>
2023-11-30 19:59:05 +0800

    完成seller

--- be/model/seller.py
+++ be/model/seller.py
@@ -23,12 +23,12 @@ class Seller(db_conn.DBConn):
             if self.book_id_exist(store_id, book_id):
                 return error.error_exist_book_id(book_id)
 
-            self.conn.execute(
+            self.cur.execute(
                 "INSERT into store(store_id, book_id, book_info, stock_level)"
-                "VALUES (?, ?, ?, ?)",
+                "VALUES (%s, %s, %s, %s)",
                 (store_id, book_id, book_json_str, stock_level),
             )
-            self.conn.commit()
+            self.cur.connection.commit()
         except sqlite.Error as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
@@ -46,12 +46,12 @@ class Seller(db_conn.DBConn):
             if not self.book_id_exist(store_id, book_id):
                 return error.error_non_exist_book_id(book_id)
 
-            self.conn.execute(
-                "UPDATE store SET stock_level = stock_level + ? "
-                "WHERE store_id = ? AND book_id = ?",
+            self.cur.execute(
+                "UPDATE store SET stock_level = stock_level + %s "
+                "WHERE store_id = %s AND book_id = %s",
                 (add_stock_level, store_id, book_id),
             )
-            self.conn.commit()
+            self.cur.connection.commit()
         except sqlite.Error as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
@@ -64,11 +64,11 @@ class Seller(db_conn.DBConn):
                 return error.error_non_exist_user_id(user_id)
             if self.store_id_exist(store_id):
                 return error.error_exist_store_id(store_id)
-            self.conn.execute(
-                "INSERT into user_store(store_id, user_id)" "VALUES (?, ?)",
+            self.cur.execute(
+                "INSERT into user_store(store_id, user_id)" "VALUES (%s, %s)",
                 (store_id, user_id),
             )
-            self.conn.commit()
+            self.cur.connection.commit()
         except sqlite.Error as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
